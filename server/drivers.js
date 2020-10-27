d1ac339772f06a156b3e5ca0754bcc2eaf6891c6
Kais Temimi <kassus007@gmail.com>
2020-10-26 21:20:30 -0800

    prepared the server of drivers and users

---
+++ server/drivers.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const db = require('../database/server.js');
+const router = express.Router();
+
+
+router.post('/signup', (req, res) => {
+    // for client side information to insert into database as sign up
+});
+
+
+
+router.post('/signin', (req, res) => {
+    // for client side information to check fo sign in
+});
+
+
+
+
+
+
+module.exports = router;
